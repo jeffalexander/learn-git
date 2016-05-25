94e5f9459977a476c231cc248004ae01d129f887
jeffalexander <jeffrey.alan.alexander@gmail.com>
2016-05-24 20:19:13 -0400

    can now choose font size

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,7 +3,7 @@ public class MainTest {
 	
 	public static void main(String[] args) {
 		Hello hello = new Hello();
-		int quantity = hello.getQuantity();
-		System.out.println("The quanitity is: " + quantity);
+		System.out.println("The quanitity is: " + hello.getQuantity());
+		System.out.println("The fontSize is: " + hello.getFontSize());
 	}
 }
