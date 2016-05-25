25a909422c437f7f4502c0f5cc89504147786004
jeffalexander <jeffrey.alan.alexander@gmail.com>
2016-05-24 20:31:22 -0400

    system can now show more than blanks

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,11 @@ public class Hello {
 	private String message;
 	private int quantity;
 	
+	public Hello(String message, int quantity) {
+		this.message = message;
+		this.quantity = quantity;
+	}
+	
 	public String getMessage() {
 		return message;
 	}
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,7 +2,7 @@
 public class MainTest {
 	
 	public static void main(String[] args) {
-		Hello hello = new Hello();
+		Hello hello = new Hello("world hi", 7);
 		int quantity = hello.getQuantity();
 		System.out.println("The quanitity is: " + quantity);
 	}
